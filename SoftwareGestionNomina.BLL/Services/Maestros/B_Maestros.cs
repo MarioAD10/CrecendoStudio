da4bcd37035d499e4a5cfa22ba12b665edc6111e
Mario <duranmario480@gmail.com>
2026-03-30 14:32:13 -0400

    Desarrollo de la capa entidad, datos, y de negocio para mestros

---
+++ SoftwareGestionNomina.BLL/Services/Maestros/B_Maestros.cs
@@ -0,0 +1,35 @@
+﻿using SoftwareGestionNomina.DAL.Repositories.Maestros;
+using SoftwareGestionNomina.Entities.Entities.Maestros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareGestionNomina.BLL.Services.Maestros
+{
+    public class B_Maestros
+    {
+        D_Maestros objDato = new D_Maestros();
+
+        public List<E_Maestros> ListarMaestro(string busqueda)
+        {
+            return objDato.ListarMaestro(busqueda);
+        }
+
+        public void InsertandoMaestro(E_Maestros m)
+        {
+            objDato.InsertarMaestro(m);
+        }
+
+        public void ModificandoMaestro(E_Maestros m)
+        {
+            objDato.ModificarMaestro(m);
+        }
+
+        public void EliminandoMaestro(E_Maestros m)
+        {
+            objDato.EliminarMaestro(m);
+        }
+    }
+}
